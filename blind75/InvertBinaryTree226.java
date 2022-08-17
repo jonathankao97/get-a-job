c8941dd1b6a8251018decb1fb968d8b75855c184
Jonathan Kao <jkao97@yahoo.com>
2022-08-17 16:19:03 -0700

    feat: implement 226

---
+++ blind75/InvertBinaryTree226.java
@@ -0,0 +1,33 @@
+public class InvertBinaryTree226 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        };
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        return root;
+    }
+}
