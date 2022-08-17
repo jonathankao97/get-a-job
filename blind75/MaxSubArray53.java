c7063e504456c856f08a3ad36076c1889e2e6225
Jonathan Kao <jkao97@yahoo.com>
2022-08-17 16:43:36 -0700

    feat: implement 53

---
+++ blind75/MaxSubArray53.java
@@ -0,0 +1,14 @@
+public class MaxSubArray53 {
+    public static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
